a725e79f13078d6d88602c6f5248a687f9b3deb1
Julian Armstrong <julian.armstrong8@gmail.com>
2024-08-08 09:29:32 -0600

    completed exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite Holiday? ");
+            string holiday = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal? ");
+            string favAnimal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band? ");
+            string favBand = Console.ReadLine();
+            Console.WriteLine("What is your favorite venue? ");
+            string venue = Console.ReadLine();
+            
+            Console.WriteLine($"{name} took thier pet {favAnimal} to see {favBand} at the legendary {venue} for the {holiday} festival!");
         }
     }
 }
